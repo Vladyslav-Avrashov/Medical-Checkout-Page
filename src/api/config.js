abba8d01e91aaed50af7b6ed607d91e8c35a2c33
Владислав <vladisloveavrashov@gmail.com>
2025-09-24 03:55:44 +0300

    Update config.js

--- src/api/config.js
+++ src/api/config.js
@@ -1,8 +1,6 @@
 import axios from "axios";
 
-// const API_BASE_URL = "https://medical-checkout-page-backend.onrender.com";
-
-const API_BASE_URL = "http://localhost:3000";
+const API_BASE_URL = "https://medical-checkout-page-backend.onrender.com";
 
 const apiClient = axios.create({
   baseURL: API_BASE_URL,
